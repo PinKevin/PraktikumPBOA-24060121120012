65ce31f7e1e3412b858608d801a4c83a91025e58
Emerio Kevin Aryaputra <emeriokevin2320@gmail.com>
2023-04-01 10:18:14 +0700

    Mengubah variabel "l" menjadi "lingkaran"
    Perubahan dilakukan agar kode mudah dipahami

--- Pertemuan 5/MLingkaran.java
+++ Pertemuan 5/MLingkaran.java
@@ -11,7 +11,7 @@ public class MLingkaran {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Masukkan jejari lingkaran : ");
 		double jejari = scan.nextDouble();
-		Lingkaran l = new Lingkaran(jejari); //diubah
-		System.out.println("Luas lingkaran dengan jejari " + jejari + " satuan adalah " + l.hitungLuas()); //diubah
+		Lingkaran lingkaran = new Lingkaran(jejari); 
+		System.out.println("Luas lingkaran dengan jejari " + jejari + " satuan adalah " + lingkaran.hitungLuas()); 
 	}
 }
